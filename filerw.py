1b003191af5f0d0aaa09735b3e037c57bde48489
杨怀志 <aidedaijiayang@126.com>
2016-11-20 13:51:15 +0800

    增加说明

--- filerw.py
+++ filerw.py
@@ -5,7 +5,7 @@ Created on Sun Nov 20 09:57:15 2016
 
 @author: yang
 """
-
+#生成Blowing in the wind.txt文件
 f = open(r'Blowing in the wind.txt','w')
 
 f.writelines('How many roads must a man walk down\n')
@@ -26,6 +26,7 @@ f.writelines('The answer is blowing in the wind\n')
 
 f.close()
 
+#读取该文件,添加专辑头,添加每首歌的作者,添加专辑尾
 g = open(r'Blowing in the wind.txt','r')
 
 songs = g.readlines()
@@ -39,6 +40,7 @@ h.writelines('Blowin\' in the wind\r\n')
 #    h.write(' Bob Dylan')
 
 for i in range(0, len(songs)):
+    #要用strip函数来清除换行符
     songs[i] = songs[i].strip() + ' - Bob Dylan\n'
     print(songs[i])
 
